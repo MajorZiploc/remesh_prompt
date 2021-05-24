f85ea1b407537325a156d63f99a50f811ec607e8
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-24 17:12:11 -0500

    Separates test cases into files

---
+++ server/remesh/utils_for_tests.py
@@ -0,0 +1,29 @@
+from remesh.models import Team, Conversation
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+
+def create_user(username, password):
+  user = User.objects.create_user(username=username, password=password)
+  return user
+
+
+def get_user_creds():
+  return 'testuser', '12345'
+
+
+def create_team(user):
+  t = Team.objects.create()
+  t.members.set([user])
+  return t
+
+def create_conversation(moderator, team):
+  return Conversation.objects.create(
+      title='Pancakes',
+      date=datetime.now(),
+      start_time=datetime.now(),
+      duration=timedelta(
+          days=2),
+      max_num_of_participants=50,
+      moderator=moderator,
+      team=team)
+
