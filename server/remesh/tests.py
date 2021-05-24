0930f13dc9a8c2e54f9918d5f8459059ebfad945
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-24 14:21:28 -0500

    Refactors create_user in test cases

--- server/remesh/tests.py
+++ server/remesh/tests.py
@@ -7,10 +7,12 @@ from django.contrib.messages import get_messages
 import datetime
 
 
-def create_user(client):
-  user = User.objects.create_user(username='testuser', password='12345')
-  login = client.login(username='testuser', password='12345')
-  return user, login
+def create_user(username, password):
+  user = User.objects.create_user(username=username, password=password)
+  return user
+
+def get_user_creds():
+  return 'testuser', '12345'
 
 
 def create_team(user):
@@ -21,14 +23,18 @@ def create_team(user):
 
 class TeamIndexViewTests(TestCase):
   def test_no_teams(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     response = self.client.get(reverse('remesh:team_index'))
     self.assertEqual(response.status_code, 200)
     self.assertContains(response, "No teams are available.")
     self.assertQuerysetEqual(response.context['team_list'], [])
 
   def test_shows_teams(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     team1 = create_team(user=user)
     response = self.client.get(reverse('remesh:team_index'))
     self.assertEqual(response.status_code, 200)
@@ -37,12 +43,16 @@ class TeamIndexViewTests(TestCase):
 
 class TeamDetailViewTests(TestCase):
   def test_no_team_details_404(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     response = self.client.get(reverse('remesh:team_detail', args=(1,)))
     self.assertEqual(response.status_code, 404)
 
   def test_shows_team_details(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     team1 = create_team(user=user)
     response = self.client.get(reverse('remesh:team_detail', args=(team1.id,)))
     self.assertEqual(response.status_code, 200)
@@ -50,7 +60,9 @@ class TeamDetailViewTests(TestCase):
 
 class TeamAddFormTests(TestCase):
   def test_add_team_form_exists(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     response = self.client.get(reverse('remesh:team_add'))
     self.assertEqual(response.status_code, 200)
     labels = [
@@ -60,7 +72,9 @@ class TeamAddFormTests(TestCase):
       self.assertContains(response, label)
 
   def test_add_team_form_post_for_valid_data(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     response = self.client.post(
       reverse('remesh:team_add'),
       data={
@@ -74,12 +88,16 @@ class TeamAddFormTests(TestCase):
 
 class TeamEditFormTests(TestCase):
   def test_edit_team_form_404_with_invalid_team_id(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     response = self.client.get(reverse('remesh:team_edit', args=(1,)))
     self.assertEqual(response.status_code, 404)
 
   def test_edit_team_form_post_for_valid_data(self):
-    user, login = create_user(self.client)
+    username, password = get_user_creds()
+    user = create_user(username, password)
+    login = self.client.login(username=username, password=password)
     team1 = create_team(user=user)
     response = self.client.post(
       reverse('remesh:team_edit', args=(1,)),
