12bc966d7eef7d1a5e9485983e4965604990e96d
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-30 14:44:42 -0500

    Lints project

--- server/remesh/migrations/0003_auto_20210530_1428.py
+++ server/remesh/migrations/0003_auto_20210530_1428.py
@@ -5,21 +5,21 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('remesh', '0002_auto_20210530_1053'),
-    ]
+  dependencies = [
+      ('remesh', '0002_auto_20210530_1053'),
+  ]
 
-    operations = [
-        migrations.AlterModelOptions(
-            name='conversation',
-            options={'ordering': ['start_date_time', 'title']},
-        ),
-        migrations.AlterModelOptions(
-            name='message',
-            options={'ordering': ['date_time_sent', 'text']},
-        ),
-        migrations.AlterModelOptions(
-            name='thought',
-            options={'ordering': ['date_time_sent', 'text']},
-        ),
-    ]
+  operations = [
+      migrations.AlterModelOptions(
+          name='conversation',
+          options={'ordering': ['start_date_time', 'title']},
+      ),
+      migrations.AlterModelOptions(
+          name='message',
+          options={'ordering': ['date_time_sent', 'text']},
+      ),
+      migrations.AlterModelOptions(
+          name='thought',
+          options={'ordering': ['date_time_sent', 'text']},
+      ),
+  ]
